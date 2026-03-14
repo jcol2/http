45ddb7f0620fce117f48dc5d7a1f508888a72787
Jimmy Collins <slimjim49j@gmail.com>
2026-03-13 19:38:38 -0500

    Init, super simple http server in main.c, copy in mime.c stuff from other repo

---
+++ README.md
@@ -0,0 +1,1 @@
+# WIP Windows HTTP Server Library
---
+++ build.bat
@@ -0,0 +1,6 @@
+@echo off
+
+cl /W3 /Zi /Od /nologo example.c /link /INCREMENTAL:NO
+cl /W3 /Zi /Od /nologo mime_test.c /link /INCREMENTAL:NO
+cl /W3 /Zi /Od /nologo main.c /link /INCREMENTAL:NO
+del *.obj
